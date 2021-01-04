d586fedf4eac105547ecf4cf81fcfe40af8ef4f0
Sparsh Bhardwaj <Sparsh.Bhardwaj@xcelenergy.com>
2021-01-04 15:16:24 +0530

    Updated the data path for reading file

--- gatling-dataDrivenFramework/src/test/scala/com/knoldus/DataDriven.scala
+++ gatling-dataDrivenFramework/src/test/scala/com/knoldus/DataDriven.scala
@@ -4,11 +4,11 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
-class DataDriven extends Simulation {
+class DataDriven extends Simulation { // class extends the Gatling Simulation class 
 
-  val filePath = "/home/knoldus/GatlingCourse/src/test/resources/data/state.csv"
+  val filePath = "/home/knoldus/Data-driven-framework-for-gatling/gatling-dataDrivenFramework/src/test/resources/data/state.csv"
 
-  val httpProtocol: HttpProtocolBuilder = http.baseUrl("https://api.openbrewerydb.org")
+  val httpProtocol: HttpProtocolBuilder = http.baseUrl("https://api.openbrewerydb.org") //httpProtocol config that indicates the baseURL 
   val state = csv(filePath).circular // reads data from csv file .
   val scn = scenario("anything")
     .feed(state) // feeds the csv file into the simulation
@@ -20,5 +20,5 @@ class DataDriven extends Simulation {
       print(a)
       session}
 
-  setUp(scn.inject(atOnceUsers(4)).protocols(httpProtocol))
+  setUp(scn.inject(atOnceUsers(4)).protocols(httpProtocol)) // setup will run the test with injection profile atOnceUser
 }
